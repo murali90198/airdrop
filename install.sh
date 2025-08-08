4dd83c9a20715c3abea2e1b84728ea7aa361ac20
xVoLAnD <xvoland@gmail.com>
2025-08-09 02:45:58 +0300

    update install.sh

--- install.sh
+++ install.sh
@@ -7,19 +7,17 @@ TARGET_PATH="$TARGET_DIR/$BIN_NAME"
 
 echo "Installing $BIN_NAME to $TARGET_DIR ..."
 
-# check root rights or required sudo
 if [[ $EUID -ne 0 ]]; then
   echo "This script requires root privileges. Please run with sudo."
   exit 1
 fi
 
-# create directory
-mkdir -p "$TARGET_DIR"
+# mkdir -p "$TARGET_DIR"
 
-# copy
 cp -f "./$BIN_NAME" "$TARGET_PATH"
 
-# make rights for running +x
 chmod 755 "$TARGET_PATH"
+
+xattr -d com.apple.quarantine "$TARGET_PATH" 2>/dev/null || true
 
 echo "Installed successfully to $TARGET_PATH"
